51d791d92d148b960c00554a1e4cd449927a39a1
suneelankavarapu <suneelanakavarapu@gmail.com>
2024-04-15 16:35:45 +0530

    new file added

---
+++ Task 3 weather forecast website/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "dbe3e7f33cdda862b7a0692d4a737fc9",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
